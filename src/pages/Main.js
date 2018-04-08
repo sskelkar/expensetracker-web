88ad3c98c3ef18235d61ba2b83ccbdce4156df2f
RahulBaphana <rahulkb@thoughtworks.com>
2018-04-08 14:30:16 +0530

    added style for body

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,13 +4,15 @@ import Dashboard from "../containers/Dashboard";
 import {Header} from "../components/Header";
 import {NotFound} from "./NotFound";
 import {Report} from "../components/Report";
+import {Grid} from "react-bootstrap";
 
-export const Main = () =>
-    <main>
-        <Header/>
+export const Main = () => <main>
+    <Header/>
+    <Grid>
         <Switch>
             <Route exact path="/" component={Dashboard}/>
             <Route path="/report" component={Report}/>
             <Route component={NotFound}/>
         </Switch>
-    </main>;
+    </Grid>
+</main>

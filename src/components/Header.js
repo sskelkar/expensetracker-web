28f80118ac712c018b0c8b7d5b7f50859f468199
RahulBaphana <rahulkb@thoughtworks.com>
2018-04-08 14:05:40 +0530

    added styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 export const Header = () => {
     return (
-        <div>
-            <Link to="/">
-                <strong>ExpenseTracker</strong>
-            </Link>
-            <p/>
-            <Link to="/report">
-                <strong>Reports</strong>
-            </Link>
-        </div>
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link to="/">ExpenseTracker</Link>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem href="/report">
+                    Reports
+                </NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavDropdown eventKey={3} title="User" id="basic-nav-dropdown">
+                    <MenuItem eventKey={3.1}>Profile</MenuItem>
+                    <MenuItem eventKey={3.2}>Settings</MenuItem>
+                    <MenuItem eventKey={3.3}>Help</MenuItem>
+                    <MenuItem divider/>
+                    <MenuItem eventKey={3.4}>Feedback</MenuItem>
+                </NavDropdown>
+            </Nav>
+        </Navbar>
     )
 };
